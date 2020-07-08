8d0dfaf1d0ef7f3e181935d0bc5a599504a4a49f
Rohan Chikkam <chikkamrohan@protonmail.com>
2020-07-08 14:23:26 -0700

    Added Release Phase SQLDAL

---
+++ Prawn-King-Release/MySQLDAL/MySQLDAL/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static MySQLDAL.SQLDAL;
+
+namespace MySQLDAL
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Opening Connection to SQL Server, Enter User Credentials. Enter 'x' to create a new user.");
+            Console.Write("Username: ");
+            string password = "";
+            string username = Console.ReadLine();
+
+            if (username == "x")
+            {
+                Console.Write("Username: ");
+                string user = Console.ReadLine();
+                Console.Write("Password: ");
+                string pwd = Console.ReadLine();
+                initInsecureConn();
+                if (newUserSP(user, pwd))
+                {
+                    Console.WriteLine("User Created");
+                    username = user;
+                    password = pwd;
+                }
+                else
+                {
+                    Console.WriteLine("Fail");
+
+                }
+                closeConn();
+            }
+            else
+            {
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+            }
+
+            int userID = initConn(username,password);
+            //Console.Clear();
+
+            if (userID != 0)
+            {
+                Console.WriteLine("Connection Opened, User Credetials Correct");
+                Console.WriteLine("User ID is " + userID);
+            }
+            else
+            {
+                Console.WriteLine("USER DOES NOT EXIST");
+                //Environment.Exit(0);
+            }    
+            while (true)
+            {
+                Console.WriteLine("Enter a choice");
+                Console.WriteLine("1 - Exit");
+                Console.WriteLine("2 - UserExists?");
+                Console.WriteLine("3 - Add User Info?");
+                Console.WriteLine("4 - Create Tank");
+                Console.WriteLine("5 - Add Tank Reference");
+                Console.WriteLine("6 - Make Another User an Admin");
+                Console.WriteLine("7 - Check what tanks you have access to");
+                Console.WriteLine("8 - Check your info");
+                Console.WriteLine("9 - Check data in a tank");
+                string userInput = Console.ReadLine();
+                if(userInput == "1")
+                {
+                    break;
+                }
+                else if(userInput == "2")
+                {
+                    Console.Write("Enter username to check: ");
+                    if(usrExistSP(Console.ReadLine()))
+                    {
+                        Console.WriteLine("User Exists");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No User Found");
+
+                    }
+                }
+                else if(userInput == "3")
+                {
+                    Console.Write("Enter First Name:");
+                    string fname = Console.ReadLine();
+                    Console.Write("Enter Last Name:");
+                    string lname = Console.ReadLine();
+                    Console.Write("Enter Phone Number:");
+                    string pnumber = Console.ReadLine();
+                    if (usrInfoSP(fname,lname,pnumber))
+                    {
+                        Console.WriteLine("User Details Added Successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+
+                    }
+                }
+                else if(userInput == "4")
+                {
+                    if (addTankSP())
+                    {
+                        Console.WriteLine("New Tank Created and User Relation added");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+
+                    }
+                }
+                else if (userInput == "5")
+                {
+                    Console.Write("Enter User Id of User to Add: ");
+                    int u2add = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Tank Id to give access to: ");
+                    int tid = Convert.ToInt32(Console.ReadLine());
+                    if (addTankRefSP(u2add,tid))
+                    {
+                        Console.WriteLine("New User Relation added");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+
+                    }
+                }
+                else if (userInput == "6")
+                {
+                    Console.Write("Enter user id of user to promote to admin: ");
+                    if (mkAdminSP(Convert.ToInt32(Console.ReadLine())))
+                    {
+                        Console.WriteLine("User Promoted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed");
+
+                    }
+                }
+                else if (userInput == "7")
+                {
+                    string access = tAccessSP();
+                    if (access != "")
+                    {
+                        Console.WriteLine("You have access to tanks: " + access);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have access to no tanks, or fail.");
+
+                    }
+                }
+                else if (userInput == "8")
+                {
+                    string result = getPInfoSP();
+                    if(result == "")
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    else
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+                else if(userInput == "9")
+                {
+                    Console.WriteLine("Enter Tank ID to retrieve data");
+                    int tid = Convert.ToInt32(Console.ReadLine());
+                    string result = getTDataSP(tid);
+                    if (result == "")
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    else
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+                else if(userInput == "10")
+                {
+                    Console.WriteLine("Enter Tank ID to update data");
+                    int tid = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Data");
+                    string data = Console.ReadLine();
+                    Console.WriteLine("Enter Comment");
+                    string comments = Console.ReadLine();
+                    if(updTDataSP(tid,data,comments))
+                    {
+                        Console.WriteLine("Success");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+
+
+            Thread.Sleep(500);
+                Console.Clear();
+            }
+
+
+
+            if (closeConn())
+            {
+                Console.WriteLine("Connection Closed Succesfully");
+            }
+            Thread.Sleep(500);
+        }
+    }
+}
---
+++ Prawn-King-Release/MySQLDAL/MySQLDAL/SQLDAL.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLDAL
+{
+    class SQLDAL
+    {
+        static string username;
+        static string password;
+        static int userID;
+        static MySqlConnection conn;
+        static readonly string myConnectionString = "server=vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;database=pdl23bwz2d3ujvr6;uid=y30bd17nh2p6vncj;pwd=kjiqz5ht51pm4i6h;";
+
+        public static bool initInsecureConn()
+        {
+            conn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                Console.Write(ex);
+                return false;
+            }
+
+        }
+
+        public static int initConn(string usr, string pwd)
+        {
+            conn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                conn.Open();
+                username = usr;
+                password = pwd;
+                userID = getIDSP();
+                return userID;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                Console.Write(ex);
+                return 0;
+            }
+
+        }
+
+        public static bool closeConn()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                return false;
+            }
+
+        }
+
+        public static int getIDSP()
+        {
+            int rtnVal = 0;
+
+            MySqlCommand getID = new MySqlCommand();
+            getID.Connection = conn;
+            getID.CommandType = CommandType.StoredProcedure;
+            getID.CommandText = "getid";
+
+            MySqlParameter UserPara = new MySqlParameter("Usr", MySqlDbType.VarChar, 50);
+            UserPara.Value = username;
+            getID.Parameters.Add(UserPara);
+
+            MySqlParameter PwdPara = new MySqlParameter("Pwd", MySqlDbType.VarChar, 50);
+            PwdPara.Value = password;
+            getID.Parameters.Add(PwdPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Int64);
+            RtnPara.Value = 0;
+            RtnPara.Direction = ParameterDirection.Output;
+            getID.Parameters.Add(RtnPara);
+
+            getID.ExecuteNonQuery();
+
+            getID.Dispose();
+
+            if (getID.Parameters["Rtn"].Value != null)
+            {
+                rtnVal = Convert.ToInt32(getID.Parameters["Rtn"].Value);
+            }
+            else
+            {
+                rtnVal = 0;
+            }
+            return rtnVal;
+        }
+
+        public static bool usrExistSP(string queryUser)
+        {
+            bool rtnVal = false;
+
+            MySqlCommand usrExist = new MySqlCommand();
+            usrExist.Connection = conn;
+            usrExist.CommandType = CommandType.StoredProcedure;
+            usrExist.CommandText = "userexists";
+
+            MySqlParameter UserPara = new MySqlParameter("Usr", MySqlDbType.VarChar, 50);
+            UserPara.Value = queryUser;
+            usrExist.Parameters.Add(UserPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Bit);
+            RtnPara.Value = false;
+            RtnPara.Direction = ParameterDirection.Output;
+            usrExist.Parameters.Add(RtnPara);
+
+            usrExist.ExecuteNonQuery();
+
+            if(usrExist.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            usrExist.Dispose();
+
+            return rtnVal;
+        }
+
+        public static bool usrInfoSP(string fn, string ln, string pnum)
+        {
+            bool rtnVal = false;
+
+            MySqlCommand usrInfo = new MySqlCommand();
+            usrInfo.Connection = conn;
+            usrInfo.CommandType = CommandType.StoredProcedure;
+            usrInfo.CommandText = "uinfo";
+
+            MySqlParameter UserPara = new MySqlParameter("Usr", MySqlDbType.VarChar, 50);
+            UserPara.Value = username;
+            usrInfo.Parameters.Add(UserPara);
+
+            MySqlParameter PwdPara = new MySqlParameter("Pwd", MySqlDbType.VarChar, 50);
+            PwdPara.Value = password;
+            usrInfo.Parameters.Add(PwdPara);
+
+            MySqlParameter fname = new MySqlParameter("fname", MySqlDbType.VarChar, 50);
+            fname.Value = fn;
+            usrInfo.Parameters.Add(fname);
+
+            MySqlParameter lname = new MySqlParameter("lname", MySqlDbType.VarChar, 50);
+            lname.Value = ln;
+            usrInfo.Parameters.Add(lname);
+
+            MySqlParameter pnumber = new MySqlParameter("pnumber", MySqlDbType.VarChar, 50);
+            pnumber.Value = pnum;
+            usrInfo.Parameters.Add(pnumber);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Bit);
+            RtnPara.Value = false;
+            RtnPara.Direction = ParameterDirection.Output;
+            usrInfo.Parameters.Add(RtnPara);
+
+            usrInfo.ExecuteNonQuery();
+
+            if (usrInfo.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            usrInfo.Dispose();
+
+            return rtnVal;
+        }
+
+        public static bool addTankSP()
+        {
+            bool rtnVal = false;
+
+            MySqlCommand addTank = new MySqlCommand();
+            addTank.Connection = conn;
+            addTank.CommandType = CommandType.StoredProcedure;
+            addTank.CommandText = "addtank";
+
+            MySqlParameter UserIdPara = new MySqlParameter("UsrID", MySqlDbType.Int32);
+            UserIdPara.Value = userID;
+            addTank.Parameters.Add(UserIdPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Bit);
+            RtnPara.Value = false;
+            RtnPara.Direction = ParameterDirection.Output;
+            addTank.Parameters.Add(RtnPara);
+
+            addTank.ExecuteNonQuery();
+
+            if (addTank.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            addTank.Dispose();
+
+            return rtnVal;
+        }
+
+        public static bool addTankRefSP(int user2AddID, int tankID)
+        {
+            bool rtnVal = false;
+
+            MySqlCommand addTid = new MySqlCommand();
+            addTid.Connection = conn;
+            addTid.CommandType = CommandType.StoredProcedure;
+            addTid.CommandText = "addtref";
+
+            MySqlParameter UserIDPara = new MySqlParameter("UsrID", MySqlDbType.Int32);
+            UserIDPara.Value = userID;
+            addTid.Parameters.Add(UserIDPara);
+
+            MySqlParameter user2AddPra = new MySqlParameter("UsrAddID", MySqlDbType.Int32);
+            user2AddPra.Value = user2AddID;
+            addTid.Parameters.Add(user2AddPra);
+
+            MySqlParameter TankIDPara = new MySqlParameter("tnkID", MySqlDbType.Int32);
+            TankIDPara.Value = tankID;
+            addTid.Parameters.Add(TankIDPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Bit);
+            RtnPara.Value = false;
+            RtnPara.Direction = ParameterDirection.Output;
+            addTid.Parameters.Add(RtnPara);
+
+            addTid.ExecuteNonQuery();
+
+            if (addTid.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            addTid.Dispose();
+
+            return rtnVal;
+        }
+
+        public static bool newUserSP(string User, string Passwd)
+        {
+            bool rtnVal = false;
+
+            MySqlCommand AddUser = new MySqlCommand();
+            AddUser.Connection = conn;
+            AddUser.CommandType = CommandType.StoredProcedure;
+            AddUser.CommandText = "adduser";
+
+            MySqlParameter UserPara = new MySqlParameter("Usr", MySqlDbType.VarChar, 50);
+            UserPara.Value = User;
+            AddUser.Parameters.Add(UserPara);
+
+            MySqlParameter PasswdPara = new MySqlParameter("Pwd", MySqlDbType.VarChar, 50);
+            PasswdPara.Value = Passwd;
+            AddUser.Parameters.Add(PasswdPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Bit);
+            RtnPara.Value = false;
+            RtnPara.Direction = ParameterDirection.Output;
+            AddUser.Parameters.Add(RtnPara);
+
+            AddUser.ExecuteNonQuery();
+
+            if (AddUser.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            AddUser.Dispose();
+
+            return rtnVal;
+        }
+
+        public static bool mkAdminSP(int userID)
+        {
+            bool rtnVal = false;
+
+            MySqlCommand mkAdmin = new MySqlCommand();
+            mkAdmin.Connection = conn;
+            mkAdmin.CommandType = CommandType.StoredProcedure;
+            mkAdmin.CommandText = "mkadmin";
+
+            MySqlParameter UserPara = new MySqlParameter("Usr", MySqlDbType.VarChar, 50);
+            UserPara.Value = username;
+            mkAdmin.Parameters.Add(UserPara);
+
+            MySqlParameter PwdPara = new MySqlParameter("Pwd", MySqlDbType.VarChar, 50);
+            PwdPara.Value = password;
+            mkAdmin.Parameters.Add(PwdPara);
+
+            MySqlParameter UserIDPara = new MySqlParameter("UsrID", MySqlDbType.VarChar, 50);
+            UserIDPara.Value = userID;
+            mkAdmin.Parameters.Add(UserIDPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.Int64);
+            RtnPara.Value = 0;
+            RtnPara.Direction = ParameterDirection.Output;
+            mkAdmin.Parameters.Add(RtnPara);
+
+            mkAdmin.ExecuteNonQuery();
+
+            if (mkAdmin.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            mkAdmin.Dispose();
+
+            return rtnVal;
+        }
+
+
+        public static string tAccessSP()
+        {
+            string rtnVal = "";
+
+            MySqlCommand tAccess = new MySqlCommand();
+            tAccess.Connection = conn;
+            tAccess.CommandType = CommandType.StoredProcedure;
+            tAccess.CommandText = "taccess";
+
+            MySqlParameter UserPara = new MySqlParameter("UsrID", MySqlDbType.Int32);
+            UserPara.Value = Convert.ToInt32(userID);
+            tAccess.Parameters.Add(UserPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.VarChar,255);
+            RtnPara.Value = "";
+            RtnPara.Direction = ParameterDirection.Output;
+            tAccess.Parameters.Add(RtnPara);
+
+            tAccess.ExecuteNonQuery();
+
+            if (tAccess.Parameters["Rtn"].Value.ToString() != null)
+            {
+                rtnVal = tAccess.Parameters["Rtn"].Value.ToString();
+            }
+
+            tAccess.Dispose();
+
+            return rtnVal;
+        }
+
+        ///////////////////
+        ///
+        public static string getPInfoSP()
+        {
+            string rtnVal = "";
+
+            MySqlCommand getPInfo = new MySqlCommand();
+            getPInfo.Connection = conn;
+            getPInfo.CommandType = CommandType.StoredProcedure;
+            getPInfo.CommandText = "getPInfo";
+
+            MySqlParameter UserPara = new MySqlParameter("Usr", MySqlDbType.VarChar, 255);
+            UserPara.Value = username;
+            getPInfo.Parameters.Add(UserPara);
+
+            MySqlParameter PwdPara = new MySqlParameter("Pwd", MySqlDbType.VarChar, 255);
+            PwdPara.Value = password;
+            getPInfo.Parameters.Add(PwdPara);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.VarChar, 255);
+            RtnPara.Value = "";
+            RtnPara.Direction = ParameterDirection.Output;
+            getPInfo.Parameters.Add(RtnPara);
+
+            getPInfo.ExecuteNonQuery();
+
+            if (getPInfo.Parameters["Rtn"].Value.ToString() != null)
+            {
+                rtnVal = getPInfo.Parameters["Rtn"].Value.ToString();
+            }
+
+            getPInfo.Dispose();
+
+            return rtnVal;
+
+        }
+
+        public static string getTDataSP(int tid)
+        {
+            string rtnVal = "";
+
+            MySqlCommand getTData = new MySqlCommand();
+            getTData.Connection = conn;
+            getTData.CommandType = CommandType.StoredProcedure;
+            getTData.CommandText = "getTData";
+
+            MySqlParameter TankID = new MySqlParameter("TID", MySqlDbType.VarChar, 255);
+            TankID.Value = tid;
+            getTData.Parameters.Add(TankID);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.VarChar, 255);
+            RtnPara.Value = "";
+            RtnPara.Direction = ParameterDirection.Output;
+            getTData.Parameters.Add(RtnPara);
+
+            getTData.ExecuteNonQuery();
+
+            if (getTData.Parameters["Rtn"].Value.ToString() != null)
+            {
+                rtnVal = getTData.Parameters["Rtn"].Value.ToString();
+            }
+
+            getTData.Dispose();
+
+            return rtnVal;
+
+        }
+
+        public static bool updTDataSP(int tid, string data, string comments)
+        {
+            bool rtnVal = false;
+
+            MySqlCommand updTData = new MySqlCommand();
+            updTData.Connection = conn;
+            updTData.CommandType = CommandType.StoredProcedure;
+            updTData.CommandText = "updTData";
+
+            MySqlParameter UserID = new MySqlParameter("UID", MySqlDbType.VarChar, 255);
+            UserID.Value = userID;
+            updTData.Parameters.Add(UserID);
+
+            MySqlParameter TankID = new MySqlParameter("TID", MySqlDbType.VarChar, 255);
+            TankID.Value = tid;
+            updTData.Parameters.Add(TankID);
+
+            MySqlParameter Data = new MySqlParameter("UData", MySqlDbType.VarChar, 255);
+            Data.Value = data;
+            updTData.Parameters.Add(Data);
+
+            MySqlParameter Comments = new MySqlParameter("Comment", MySqlDbType.VarChar, 255);
+            Comments.Value = comments;
+            updTData.Parameters.Add(Comments);
+
+            MySqlParameter RtnPara = new MySqlParameter("Rtn", MySqlDbType.VarChar, 255);
+            RtnPara.Value = "";
+            RtnPara.Direction = ParameterDirection.Output;
+            updTData.Parameters.Add(RtnPara);
+
+            updTData.ExecuteNonQuery();
+
+            if (updTData.Parameters["Rtn"].Value.ToString() == "1")
+            {
+                rtnVal = true;
+            }
+
+            updTData.Dispose();
+
+            return rtnVal;
+
+        }
+
+    }
+}
