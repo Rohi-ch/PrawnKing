0fad19017def46962e44ad330a892c483552649e
Rohan Chikkam <chikkamrohan@protonmail.com>
2020-07-08 14:28:53 -0700

    Added Release Phase Client App

---
+++ Prawn-King-Release/PrawnKingClientRelease/PrawnKingClientRelease/PrawnKingClientRelease/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PrawnKingClientRelease
+{
+    partial class ClientDash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientDash));
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.TanksButton = new System.Windows.Forms.PictureBox();
+            this.LogoPic = new System.Windows.Forms.PictureBox();
+            this.SubMenuPanel = new System.Windows.Forms.Panel();
+            this.DragPanelRight = new System.Windows.Forms.Panel();
+            this.MinimiseButton = new System.Windows.Forms.Button();
+            this.CloseApp = new System.Windows.Forms.Button();
+            this.DragPanelLeft = new System.Windows.Forms.Panel();
+            this.SummaryPanel = new System.Windows.Forms.Panel();
+            this.MenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TanksButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPic)).BeginInit();
+            this.DragPanelRight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.MenuPanel.Controls.Add(this.pictureBox3);
+            this.MenuPanel.Controls.Add(this.pictureBox4);
+            this.MenuPanel.Controls.Add(this.pictureBox2);
+            this.MenuPanel.Controls.Add(this.TanksButton);
+            this.MenuPanel.Controls.Add(this.LogoPic);
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(66, 720);
+            this.MenuPanel.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(9, 658);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(9, 595);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(8, 137);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // TanksButton
+            // 
+            this.TanksButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.TanksButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.TanksButton.Image = ((System.Drawing.Image)(resources.GetObject("TanksButton.Image")));
+            this.TanksButton.Location = new System.Drawing.Point(8, 74);
+            this.TanksButton.Name = "TanksButton";
+            this.TanksButton.Size = new System.Drawing.Size(50, 50);
+            this.TanksButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TanksButton.TabIndex = 3;
+            this.TanksButton.TabStop = false;
+            // 
+            // LogoPic
+            // 
+            this.LogoPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.LogoPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LogoPic.Image = ((System.Drawing.Image)(resources.GetObject("LogoPic.Image")));
+            this.LogoPic.Location = new System.Drawing.Point(8, 11);
+            this.LogoPic.Name = "LogoPic";
+            this.LogoPic.Size = new System.Drawing.Size(50, 50);
+            this.LogoPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoPic.TabIndex = 2;
+            this.LogoPic.TabStop = false;
+            this.LogoPic.Click += new System.EventHandler(this.LogoPic_Click);
+            // 
+            // SubMenuPanel
+            // 
+            this.SubMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.SubMenuPanel.Location = new System.Drawing.Point(66, 0);
+            this.SubMenuPanel.Name = "SubMenuPanel";
+            this.SubMenuPanel.Size = new System.Drawing.Size(280, 720);
+            this.SubMenuPanel.TabIndex = 1;
+            // 
+            // DragPanelRight
+            // 
+            this.DragPanelRight.BackColor = System.Drawing.Color.Transparent;
+            this.DragPanelRight.Controls.Add(this.MinimiseButton);
+            this.DragPanelRight.Controls.Add(this.CloseApp);
+            this.DragPanelRight.Location = new System.Drawing.Point(345, 0);
+            this.DragPanelRight.Name = "DragPanelRight";
+            this.DragPanelRight.Size = new System.Drawing.Size(858, 30);
+            this.DragPanelRight.TabIndex = 4;
+            this.DragPanelRight.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dragWindow);
+            // 
+            // MinimiseButton
+            // 
+            this.MinimiseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimiseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimiseButton.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.MinimiseButton.Location = new System.Drawing.Point(803, 4);
+            this.MinimiseButton.Name = "MinimiseButton";
+            this.MinimiseButton.Size = new System.Drawing.Size(23, 23);
+            this.MinimiseButton.TabIndex = 8;
+            this.MinimiseButton.Text = "_";
+            this.MinimiseButton.UseVisualStyleBackColor = true;
+            this.MinimiseButton.Click += new System.EventHandler(this.MinimiseButton_Click);
+            // 
+            // CloseApp
+            // 
+            this.CloseApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseApp.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.CloseApp.Location = new System.Drawing.Point(831, 4);
+            this.CloseApp.Name = "CloseApp";
+            this.CloseApp.Size = new System.Drawing.Size(23, 23);
+            this.CloseApp.TabIndex = 7;
+            this.CloseApp.Text = "X";
+            this.CloseApp.UseVisualStyleBackColor = true;
+            this.CloseApp.Click += new System.EventHandler(this.CloseApp_Click);
+            // 
+            // DragPanelLeft
+            // 
+            this.DragPanelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.DragPanelLeft.Location = new System.Drawing.Point(66, 0);
+            this.DragPanelLeft.Name = "DragPanelLeft";
+            this.DragPanelLeft.Size = new System.Drawing.Size(280, 30);
+            this.DragPanelLeft.TabIndex = 6;
+            this.DragPanelLeft.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dragWindow);
+            // 
+            // SummaryPanel
+            // 
+            this.SummaryPanel.Location = new System.Drawing.Point(429, 87);
+            this.SummaryPanel.Name = "SummaryPanel";
+            this.SummaryPanel.Size = new System.Drawing.Size(858, 694);
+            this.SummaryPanel.TabIndex = 7;
+            // 
+            // ClientDash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1203, 720);
+            this.Controls.Add(this.SummaryPanel);
+            this.Controls.Add(this.DragPanelLeft);
+            this.Controls.Add(this.DragPanelRight);
+            this.Controls.Add(this.SubMenuPanel);
+            this.Controls.Add(this.MenuPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ClientDash";
+            this.Text = "ClientDash";
+            this.MenuPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TanksButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPic)).EndInit();
+            this.DragPanelRight.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel SubMenuPanel;
+        private System.Windows.Forms.PictureBox LogoPic;
+        private System.Windows.Forms.Panel DragPanelRight;
+        private System.Windows.Forms.Panel DragPanelLeft;
+        private System.Windows.Forms.Button CloseApp;
+        private System.Windows.Forms.Button MinimiseButton;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox TanksButton;
+        private System.Windows.Forms.Panel SummaryPanel;
+    }
+}
+
---
+++ Prawn-King-Release/PrawnKingClientRelease/PrawnKingClientRelease/PrawnKingClientRelease/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace PrawnKingClientRelease
+{
+    public partial class ClientDash : Form
+    {
+        //Drag Window
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        public void dragWindow(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        //End Drag Window
+        public ClientDash()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MinimiseButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void LogoPic_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
